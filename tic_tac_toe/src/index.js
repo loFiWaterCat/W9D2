b72a146174d4d3bf51743cb22585d358a09bf0c9
alan <alan001tk@gmail.com>
2022-08-09 12:12:56 -0700

    Add requirements into index.js

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -1,5 +1,5 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view')
+const Game = require('../ttt_node/game')
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
